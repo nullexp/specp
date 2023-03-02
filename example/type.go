03c4244feb03d6364e0a66ce1777f1e8ff18aede
Omid Askari Golestani <hopegolestany@gmail.com>
2023-03-02 13:04:42 +0330

    define more spec types

--- example/type.go
+++ example/type.go
@@ -3,6 +3,8 @@ package example
 import (
 	"strings"
 	"time"
+
+	"github.com/nullexp/specp"
 )
 
 type (
@@ -15,6 +17,36 @@ type (
 		Age                int
 		RelevantExperience int
 		Skills             []Skill
+	}
+
+	OlderThanSpec[T ApplicationRequest] struct {
+		specp.CompositeSpecification[ApplicationRequest]
+		age int
+	}
+
+	YoungerThanSpec[T ApplicationRequest] struct {
+		specp.CompositeSpecification[ApplicationRequest]
+		age int
+	}
+
+	NameNotEmptySpec[T ApplicationRequest] struct {
+		specp.CompositeSpecification[ApplicationRequest]
+		firstname, lastname string
+	}
+
+	ApplyTimeOlderThanSpec[T ApplicationRequest] struct {
+		specp.CompositeSpecification[ApplicationRequest]
+		duration time.Duration
+	}
+
+	AllSkillExistSpec[T ApplicationRequest] struct {
+		specp.CompositeSpecification[ApplicationRequest]
+		wantedSkills []Skill
+	}
+
+	HasMoreRelevantExperienceThanSpec[T ApplicationRequest] struct {
+		specp.CompositeSpecification[ApplicationRequest]
+		relevantExperience int
 	}
 )
 
@@ -43,3 +75,45 @@ var (
 		Skills:             []Skill{Docker, Golang},
 	}
 )
+
+func (o OlderThanSpec[T]) IsSatisfiedBy(ar ApplicationRequest) bool {
+	return ar.Age > o.age
+}
+
+func (y YoungerThanSpec[T]) IsSatisfiedBy(ar ApplicationRequest) bool {
+	return ar.Age < y.age
+}
+
+func (n NameNotEmptySpec[T]) IsSatisfiedBy(ar ApplicationRequest) bool {
+	return !isEmpty(ar.Firstname) && !isEmpty(ar.Lastname)
+}
+
+func (at ApplyTimeOlderThanSpec[T]) IsSatisfiedBy(ar ApplicationRequest) bool {
+	return time.Until(ar.ApplyTime) > at.duration
+}
+
+func (as AllSkillExistSpec[T]) IsSatisfiedBy(ar ApplicationRequest) bool {
+	skillExist := map[Skill]bool{}
+	for _, v := range ar.Skills {
+		skillExist[v] = true
+	}
+
+	for _, v := range as.wantedSkills {
+
+		if !skillExist[v] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (as HasMoreRelevantExperienceThanSpec[T]) IsSatisfiedBy(ar ApplicationRequest) bool {
+	return ar.RelevantExperience >= ar.RelevantExperience
+}
+
+func OlderThan(age int) OlderThanSpec[ApplicationRequest] {
+	spec := OlderThanSpec[ApplicationRequest]{age: age}
+	spec.Value = specp.NewCompositeSpecification[ApplicationRequest](spec)
+	return spec
+}
