238784249e56004b28df1d16d8f707bb59f32f19
Omid Askari Golestani <hopegolestany@gmail.com>
2023-03-13 18:08:21 +0330

    change file name

---
+++ example/basic_without_spec.go
@@ -0,0 +1,42 @@
+package example
+
+import (
+	"time"
+)
+
+// AccpetedForIntreview Without Specification
+func AccpetedForIntreview(application ApplicationRequest) (accepted bool) {
+
+	// Applicant age must be more and equal than 18 and less than 30
+	if application.Age < 18 || application.Age > 30 {
+		return
+	}
+
+	// Full name must not be empty
+	if isEmpty(application.Firstname) || isEmpty(application.Lastname) {
+		return
+	}
+
+	// apply time must not be older than a week day
+	if time.Until(application.ApplyTime) > Week {
+		return
+	}
+
+	// Creating a map for fast skill search
+	skillExist := map[Skill]bool{}
+	for _, v := range application.Skills {
+		skillExist[v] = true
+	}
+
+	// We expect the applicant know Golang and Docker
+	if !skillExist[Golang] || !skillExist[Docker] {
+		return
+	}
+
+	// At least 4 years of relevant experience is expected
+	if application.RelevantExperience < 4 {
+		return
+	}
+
+	return true
+}
