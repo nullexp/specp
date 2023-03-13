deb40bd4fbef45646a4f8c35852110b8429202b9
Omid Askari Golestani <hopegolestany@gmail.com>
2023-03-13 18:05:25 +0330

    change into spec form

--- example/basic_with_spec.go
+++ example/basic_with_spec.go
@@ -1,42 +1,13 @@
 package example
 
-import (
-	"time"
-)
-
 // AccpetedForIntreview Without Specification
 func AccpetedForIntreview(application ApplicationRequest) (accepted bool) {
 
-	// Applicant age must be more and equal than 18 and less than 30
-	if application.Age < 18 || application.Age > 30 {
-		return
-	}
+	return OlderThan(18).
+		And(YoungerThan(30)).
+		And(NameNotEmpty(application.Firstname, application.Lastname)).
+		And(ApplyTimeOlderThan(Week)).
+		And(AllSkillExist(Golang, Docker)).
+		And(HasMoreRelevantExperienceThan(4)).IsSatisfiedBy(application)
 
-	// Full name must not be empty
-	if isEmpty(application.Firstname) || isEmpty(application.Lastname) {
-		return
-	}
-
-	// apply time must not be older than a week day
-	if time.Until(application.ApplyTime) > Week {
-		return
-	}
-
-	// Creating a map for fast skill search
-	skillExist := map[Skill]bool{}
-	for _, v := range application.Skills {
-		skillExist[v] = true
-	}
-
-	// We expect the applicant know Golang and Docker
-	if !skillExist[Golang] || !skillExist[Docker] {
-		return
-	}
-
-	// At least 4 years of relevant experience is expected
-	if application.RelevantExperience < 4 {
-		return
-	}
-
-	return true
 }
