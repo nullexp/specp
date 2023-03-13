d622e55098e012b0f88a5415379ac8ed6af0d706
Omid Askari Golestani <hopegolestany@gmail.com>
2023-03-13 18:21:10 +0330

    merge files

--- example/basic_spec.go
+++ example/basic_spec.go
@@ -40,3 +40,15 @@ func IsAccpeted(application ApplicationRequest) (accepted bool) {
 
 	return true
 }
+
+// IsAccpetedWithSpec Wit Specification
+func IsAccpetedWithSpec(application ApplicationRequest) (accepted bool) {
+
+	return OlderThan(18).
+		And(YoungerThan(30)).
+		And(NameNotEmpty(application.Firstname, application.Lastname)).
+		And(ApplyTimeOlderThan(Week)).
+		And(AllSkillExist(Golang, Docker)).
+		And(HasMoreRelevantExperienceThan(4)).IsSatisfiedBy(application)
+
+}
